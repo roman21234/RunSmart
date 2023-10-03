3240cb1c10ec832213f95f9d382907e1f4bedf8f
Romano-laptop <krutoyarskie@yandex.ru>
2023-10-03 10:39:11 +0300

    Beginning catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
         infinite: false,
         speed: 300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron_left_solid_980.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron_right_solid_982.png"></button>',
       });
 });
 
