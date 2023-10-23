2311620edd045d1b2f73d186245a530c0090d382
romano <krutoyarskie@yandex.ru>
2023-10-24 02:44:41 +0300

    work with forms, email

--- src/js/script.js
+++ src/js/script.js
@@ -40,28 +40,60 @@ $(document).ready(function(){
         })
       })
 
-      $('#consultation-form').validate();
-      $('#consultation form').validate({
-        rules: {
-          name: "required",
-          phone: "required",
-          email: {
-            required: true,
-            email: true,
-            
+      function valideForms(form){
+        $(form).validate({
+          rules: {
+            name: {
+              required: true,
+              minlength: 2,
+            },
+            phone: "required",
+            email: {
+              required: true,
+              email: true,
+              
+            },
           },
-        },
-        messages: {
-          name: "Пожалуйста, введите свое имя",
-          phone: "Пожалуйста, введите свой телефон",
-          email: {
-            required: "Пожалуйста, введите свою электронную почту",
-            email: "Неправильно введен адрес почты"
+          messages: {
+            name: {
+              required: "Пожалуйста, введите свое имя",
+              minlength: jQuery.validator.format("Введите {0} символов!")
+            },
+            phone: "Пожалуйста, введите свой телефон",
+            email: {
+              required: "Пожалуйста, введите свою электронную почту",
+              email: "Неправильно введен адрес почты"
+            }
           }
-        }
-      });
-      $('#order form').validate();
+        });
+      }
 
+      valideForms('#consultation-form');
+      valideForms('#consultation form');
+      valideForms('#order form');
+
+      $('input[name=phone]').mask("+7 (999) 999-9999");
+
+      $('form').submit(function(e){
+        e.preventDefault(); // Отключение стандартного поведения браузера
+
+        if (!$(this).valid()){
+          return;
+        }
+
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function(){
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+        });
+        return false;
+      });
 });
 
 
